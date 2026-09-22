76dab547b156d08632ac09b9d74dc37189e8c35a
Siddhi Vinayak Dash <133249544+siddhivdash@users.noreply.github.com>
2026-09-22 23:16:52 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (38.35%) - LeetSync

---
+++ 792-binary-search/binary-search.py
@@ -0,0 +1,16 @@
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        l = 0
+        r = len(nums) -1 
+        while r >= l:
+            mid = (l+r)//2
+            if nums[mid] == target:
+                return mid
+            if target > nums[mid]:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return -1 
+        
+
+        
